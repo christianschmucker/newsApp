bc93f69a7668151e4e3bd69fd52bdeee03610d22
Christian Schmucker <37977212+christianschmucker@users.noreply.github.com>
2020-03-31 18:33:01 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Theme Provider',
+      title: 'Dot. News',
       home: Home(),
     );
   }
